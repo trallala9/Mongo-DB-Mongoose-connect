76fe4896d8916bd16abd819de554ecb1313f6fae
Karolina Siwkowska <trallala00@gmail.com>
2018-05-22 22:00:48 +0200

    Add mariochar.js,modify p.json, add demo_test.js file

---
+++ test/demo_test.js
@@ -0,0 +1,9 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+describe('some test one', function () {
+    // Ceate first test
+    it('adds two numbers', function () {
+        assert(2 + 3 === 5);
+    });
+});
