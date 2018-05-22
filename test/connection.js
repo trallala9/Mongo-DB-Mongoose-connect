e6b35281bdc8e9b0a15e0274a15c7f7240bea20e
Karolina Siwkowska <trallala00@gmail.com>
2018-05-22 22:56:46 +0200

    Change code in connetions.js

--- test/connection.js
+++ test/connection.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
-//connect to mongodb
 
-mongoose.connect('mongodb://localhost/testbase');
+//ES6 Promises if we get message about promises
+//mongoose.Promise = global.Promise;
 
-
-mongoose.connection.once('open', function () {
-    console.log('Connection established my Queen ');
-}).on('error', function (error) {
-    console.log('Connection error:', error);
+//establish connection before run the test
+before(function (done) {
+    //connect to mongodb
+    mongoose.connect('mongodb://localhost/testbase');
+    mongoose.connection.once('open', function () {
+        console.log('Connection established my Queen ');
+        done();
+    }).on('error', function (error) {
+        console.log('Connection error:', error);
+    });
 });
